1890e96f95437fb85c1f10acd65a8f5206c46bfd
mr-edr <mr.edr.enigma@gmail.com>
2026-02-20 19:19:20 +0530

    Reset repository

--- src/Strings.java
+++
@@ -1,7 +0,0 @@
-public class Strings {
-    void main() {
-        String s1 = new String("HELLO");
-        String s2 = new String("HLLO");
-        System.out.println(s2.compareTo(s1));
-    }
-}
--- src/numbox.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class numbox {
-    static void main() {
-        Scanner s = new Scanner(System.in);
-        System.out.print("E");
-        int i = s.nextInt();
-    }
-}
